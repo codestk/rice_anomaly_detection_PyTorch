7170a00b3400811eb56265ca5c0fab0c7aec8341
prasert <prasert.pom@bbl.co.th>
2026-04-18 14:12:25 +0700

    Refactor code structure for improved readability and maintainability

--- Z_VGA_TEST copy.py
+++ Z_VGA_TEST copy.py
@@ -1,5 +1,104 @@
-import torch
-print("torch cuda available:", torch.cuda.is_available())
-if torch.cuda.is_available():
-    print("cuda device:", torch.cuda.get_device_name(0))
- 
+import cv2
+import os
+
+# ===== CONFIG =====
+IMAGE_FOLDER = r"D:\rice_anomaly_detection_PyTorch\data\train"
+THRESHOLD = 20   # ⭐ ปรับตรงนี้
+# ==================
+
+def is_blurry(image_path, threshold=20):
+    image = cv2.imread(image_path)
+
+    if image is None:
+        return None
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    variance = cv2.Laplacian(gray, cv2.CV_64F).var()
+
+    return variance, variance < threshold
+
+
+def scan_folder(folder):
+    blurry_files = []
+
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            if file.lower().endswith((".jpg", ".jpeg", ".png", ".bmp")):
+                path = os.path.join(root, file)
+
+                result = is_blurry(path, THRESHOLD)
+                if result is None:
+                    continue
+
+                variance, is_blur = result
+
+                if is_blur:
+                    #filename = os.path.basename(path)
+                    filename = path
+                    blurry_files.append(filename)
+
+    return blurry_files
+
+
+if __name__ == "__main__":
+    blurry = scan_folder(IMAGE_FOLDER)
+
+    print("\n=== BLURRY IMAGES (sharpness < 20) ===")
+    for name in blurry:
+        print(name)
+
+    print(f"\nTotal blurry: {len(blurry)}")
+
+
+
+
+# import cv2
+# import os
+
+# # ===== CONFIG =====
+# IMAGE_FOLDER = r"C:\yoloTrain\custom_data\images"
+# THRESHOLD = 100
+# # ==================
+
+# def is_blurry(image_path, threshold=100):
+#     image = cv2.imread(image_path)
+
+#     if image is None:
+#         return None
+
+#     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#     variance = cv2.Laplacian(gray, cv2.CV_64F).var()
+
+#     return variance, variance < threshold
+
+
+# def scan_folder(folder):
+#     blurry_files = []
+
+#     for root, dirs, files in os.walk(folder):
+#         for file in files:
+#             if file.lower().endswith((".jpg", ".jpeg", ".png", ".bmp")):
+#                 path = os.path.join(root, file)
+
+#                 result = is_blurry(path, THRESHOLD)
+#                 if result is None:
+#                     continue
+
+#                 variance, is_blur = result
+
+#                 if is_blur:
+#                     # ⭐ เอาแค่ชื่อไฟล์
+#                     filename = os.path.basename(path)
+#                     blurry_files.append((filename, variance))
+
+#     return blurry_files
+
+
+# if __name__ == "__main__":
+#     blurry = scan_folder(IMAGE_FOLDER)
+
+#     print("\n=== BLURRY IMAGES (FILENAME ONLY) ===")
+#     for name, v in blurry:
+#         print(f"{name} | sharpness={v:.2f}")
+
+#     print(f"\nTotal blurry: {len(blurry)}")
